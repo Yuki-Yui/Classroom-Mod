6f4b8a463166a90e71eab908afdf446e08cbf4d2
Yuki-Yui <118826058+Yuki-Yui@users.noreply.github.com>
2024-12-18 17:18:14 +0900

    inital

---
+++ content.js
@@ -0,0 +1,25 @@
+const message_dict = {
+    "さんが新しい資料を投稿しました:": "資料:",
+    "さんが新しい課題を投稿しました:": "課題:"
+}
+
+
+const observer = new MutationObserver(() => {
+    const spans = document.querySelectorAll('span');  
+    spans.forEach((span) => {
+        for (const [key, value] of Object.entries(message_dict)) {
+            if (span.textContent.includes(key)) {
+                span.textContent = span.textContent.replace(
+                    new RegExp(`(.+?)${key}`),
+                    (match, name) => {
+                        const shortenedName = name.length > 5 ? name.slice(0, 5) + "…" : name;
+                        return `${shortenedName} ${value}`;
+                    }
+                );
+            }
+        }
+    });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+  
