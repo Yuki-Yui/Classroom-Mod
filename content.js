53cefd7220803c341fa9c667710673d018b76e26
Yuki-Yui <yuukiyuinyan@gmail.com>
2025-01-27 14:07:10 +0900

    v2.1 機能のON/OFFをできるように

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
+const replace_label = true; // 文章の短縮
+const direct_download = true; // 直ダウンロード
+
+const name_length = 5;
 const name_display = true;
 const label_display = true;
-const direct_download = true;
-
 const message_dict = {
     "さんが新しい資料を投稿しました": "資料",
     "さんが新しい課題を投稿しました": "課題"
 }
-const name_length = 5;
 
 const color = '#000000';
 const downloadSvg = `<svg viewbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
@@ -19,26 +20,29 @@ const downloadSvg = `<svg viewbox="0 0 200 200" xmlns="http://www.w3.org/2000/sv
 const observer = new MutationObserver(() => {
     const pageURL = window.location.href;
     const loginID = pageURL.match(/\/u\/\d+\//) ? pageURL.match(/\/u\/(\d+)\//)[1] : 0;
-    const spans = document.querySelectorAll('span');  
-    spans.forEach((span) => {
-        for (const [key, value] of Object.entries(message_dict)) {
-            if (span.textContent.includes(key)) {
-                span.textContent = span.textContent.replace(
-                    new RegExp(`(.+?)${key}`),
-                    (match, name) => {
-                        if (name_display && label_display) {
-                            const shortenedName = name.length > name_length ? name.slice(0, 5) + "…" : name;
-                            return `${shortenedName} ${value}`;
-                        } else if (label_display) {
-                            return value;
-                        } else {
-                            return '';
+
+    if (replace_label){
+        const spans = document.querySelectorAll('span');  
+        spans.forEach((span) => {
+            for (const [key, value] of Object.entries(message_dict)) {
+                if (span.textContent.includes(key)) {
+                    span.textContent = span.textContent.replace(
+                        new RegExp(`(.+?)${key}`),
+                        (match, name) => {
+                            if (name_display && label_display) {
+                                const shortenedName = name.length > name_length ? name.slice(0, 5) + "…" : name;
+                                return `${shortenedName} ${value}`;
+                            } else if (label_display) {
+                                return value;
+                            } else {
+                                return '';
+                            }
                         }
-                    }
-                );
+                    );
+                }
             }
-        }
-    });
+        });
+    }
 
     if (direct_download) {
         const driveLinks = document.querySelectorAll('a[href*="drive.google.com/file/d/"]');
