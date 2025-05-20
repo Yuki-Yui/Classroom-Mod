8f39b46f334d99d8f1e527c8f4eb899ce1f3881d
Yuki-Yui <yuukiyuinyan@gmail.com>
2025-05-20 11:27:48 +0900

    v2.2.2 ダウンロードアイコンの更新など

--- content.js
+++ content.js
@@ -10,65 +10,72 @@ const message_dict = {
 }
 
 const color = '#000000';
-const downloadSvg = `<svg viewbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" version="1.1"style="stroke:currentColor;">
-  <line x1="100" y1="20" x2="100" y2="170" stroke-width="20"/>
-  <line x1="40" y1="120" x2="100" y2="170" stroke-width="20"/>
-  <line x1="160" y1="120" x2="100" y2="170" stroke-width="20"/>
-  <line x1="20" y1="180" x2="180" y2="180" stroke-width="20"/>
+const downloadSvg = `<svg viewbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <path d="M 100 10 L 100 160" stroke="black" stroke-width="20" fill="transparent" />
+  <path d="M 40 100 L 100 160 L 160 100" stroke="black" stroke-width="20" fill="transparent" />
+  <path d="M 10 140 L 10 190 L 190 190 L 190 140" stroke="black" stroke-width="20" fill="transparent"/>
 </svg>`
 
 const observer = new MutationObserver(() => {
-    const pageURL = window.location.href;
-    const loginID = pageURL.match(/\/u\/\d+\//) ? pageURL.match(/\/u\/(\d+)\//)[1] : 0;
 
     if (replace_label){
-        const spans = document.querySelectorAll('span');  
-        spans.forEach((span) => {
-            for (const [key, value] of Object.entries(message_dict)) {
-                if (span.textContent.includes(key)) {
-                    span.textContent = span.textContent.replace(
-                        new RegExp(`(.+?)${key}`),
-                        (match, name) => {
-                            if (name_display && label_display) {
-                                const shortenedName = name.length > name_length ? name.slice(0, name_length) + "…" : name;
-                                return `${shortenedName} ${value}`;
-                            } else if (label_display) {
-                                return value;
-                            } else {
-                                return '';
-                            }
-                        }
-                    );
-                }
-            }
-        });
+        replaceLabelText();
     }
 
     if (direct_download) {
-        const driveLinks = document.querySelectorAll('a[href*="drive.google.com/file/d/"]');
-        driveLinks.forEach((driveLink) => {
-            const parentDiv = driveLink.closest('div');
-            if (parentDiv.querySelector('.direct-download') || parentDiv.querySelectorAll('a').length > 1) {
-                return;
-            }
-            const match = driveLink.href.match(/https:\/\/drive\.google\.com\/file\/d\/([^/]+)/);
-            if (match) {
-                const fileId = match[1];
-                directURL = `https://drive.google.com/u/${loginID}/uc?id=${fileId}`;
-                const directLink = document.createElement('a');
-                directLink.href = directURL;
-                directLink.style.color = "currentColor";
-                directLink.classList.add('direct-download');
-                const downloadIcon = document.createElement('div');
-                downloadIcon.innerHTML = downloadSvg;
-                downloadIcon.style.width = '1.5em';
-                downloadIcon.style.height = '1.5em';
-                directLink.appendChild(downloadIcon);
-                parentDiv.appendChild(directLink);
-            }
-        });
+       createDownloadButton();
     }
 
 });
+
+function replaceLabelText() {
+    const spans = document.querySelectorAll('span');  
+    spans.forEach((span) => {
+        for (const [key, value] of Object.entries(message_dict)) {
+            if (span.textContent.includes(key)) {
+                span.textContent = span.textContent.replace(
+                    new RegExp(`(.+?)${key}`),
+                    (match, name) => {
+                        if (name_display && label_display) {
+                            const shortenedName = name.length > name_length ? name.slice(0, name_length) + "…" : name;
+                            return `${shortenedName} ${value}`;
+                        } else if (label_display) {
+                            return value;
+                        } else {
+                            return '';
+                        }
+                    }
+                );
+            }
+        }
+    });
+}
+
+function createDownloadButton() {
+    const pageURL = window.location.href;
+    const loginID = pageURL.match(/\/u\/\d+\//) ? pageURL.match(/\/u\/(\d+)\//)[1] : 0;
+    const driveLinks = document.querySelectorAll('a[href*="drive.google.com/file/d/"]');
+    driveLinks.forEach((driveLink) => {
+        const parentDiv = driveLink.closest('div');
+        if (parentDiv.querySelector('.direct-download') || parentDiv.querySelectorAll('a').length > 1) {
+            return;
+        }
+        const match = driveLink.href.match(/https:\/\/drive\.google\.com\/file\/d\/([^/]+)/);
+        if (match) {
+            const fileId = match[1];
+            directURL = `https://drive.google.com/u/${loginID}/uc?id=${fileId}`;
+            const directLink = document.createElement('a');
+            directLink.href = directURL;
+            directLink.style.color = "currentColor";
+            directLink.classList.add('direct-download');
+            const downloadIcon = document.createElement('div');
+            downloadIcon.innerHTML = downloadSvg;
+            downloadIcon.style.width = '1.5em';
+            downloadIcon.style.height = '1.5em';
+            directLink.appendChild(downloadIcon);
+            parentDiv.appendChild(directLink);
+        }
+    });
+}
 
 observer.observe(document.body, { childList: true, subtree: true });
